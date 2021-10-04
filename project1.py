2b01a35d2be277c61343d0ba44effdaa5cfb3bf6
Sigurd Berg Svela <sigsve@Sigurds-MBP.home>
2021-10-04 15:39:37 +0200

    Fix some things
    Remove debug code
    add loads of comments
    Fix some things
    
    Remove debug code
    add loads of comments

--- project1.py
+++ project1.py
@@ -3,6 +3,8 @@ import random
 
 # Start Screen
 def main():
+
+	# Print the game rules
 	print("Welcome to the Game! Here are the rules:")
 	print("- Choose any number between 1 and 50.")
 	print("- If you guess the number correctly at your 1st attempt, you will get 10 times of money you bet.")
@@ -13,18 +15,22 @@ def main():
 
 	name = input("Please enter your name:")
 
+	# Get the initial balance
 	while True:
 		balance = int(input("Please enter your initial balance:"))
 		if not balance > 0:
 			print("Balance must be greater than 0. Please try again")
 		else:
 			break
-
+	
+	# Start the game
 	play_round(name, balance)
 
 def play_round(name, balance):
 	print()
+
 	bet = 0
+
 	# Enter the betting amount
 	while True:
 		bet = int(input("Enter bet (Balance=" + str(balance) +  "):"))
@@ -37,18 +43,23 @@ def play_round(name, balance):
 
 	print("All right, lets play!")
 
+	# Remove the bet from the player balance
 	new_balance = balance - bet
 
-	#answer = random.randint(1, 50)
-	answer = 25
+	# Generate the answer
+	answer = random.randint(1, 50)
 	
-	rewards = [10, 5, 3]
-
+	# True if the player guessed the correct answer
 	won = False
-	attempt = 0
-	while attempt <= 3:
-		attempt+=1
 
+	# Initilize the attempt counter
+	attempt = 0
+
+	# Guess loop
+	while attempt <= 3:
+		attempt += 1 # Increment attempt counter
+
+		# Enter the guess
 		while True:
 			guess = int(input("Guess an integer between 1 and 50:"))
 			if guess < 1 or guess > 50:
@@ -61,12 +72,21 @@ def play_round(name, balance):
 		elif guess < answer:
 			print("Your guess was to low")
 		else:
+			# If the guess was not to high, and not to low
+			# it has to be right
 			won = True
+
+			# Break from the loop
 			break
 	
+	# Check if the player guessed the right number
 	if won:
+		# Calculate the amount the player won
 		amount = math.floor(10 / float(attempt)) * bet
+		
+		# Add the amount to the new balance of the player
 		new_balance += amount
+
 		print("Congratulation! You win " + str(amount) + "$")
 	else:
 		print("Sorry!! You lost your betting amount, " + str(bet) + "$")
@@ -74,15 +94,19 @@ def play_round(name, balance):
 
 	play_again(name, new_balance)
 
-
+# Ask the player if they want to play again
 def play_again(name, balance):
 	print()
-	if balance == 0: # It should never be able to be less than 0
+
+	# Check if the player is out of money
+	if balance == 0:
 		print("You are out of money,")
 		print("Good bye.")
+	# If not, ask them if they would like to play again
 	elif input("Would you like to play again? [Y/n] ") == "n":
 		print("All right,")
 		print("Good bye.")
+	# If they do, lets play
 	else:
 		play_round(name, balance)
 
